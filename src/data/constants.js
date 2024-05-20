a53121fa26cda47bf926aaf5f755d0a58ebbdc3a
sasikiran0 <sasikran@bloomitsolution.in>
2024-05-20 16:32:20 +0530

    new resume added

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sasikola",
   resume:
-    "https://drive.google.com/file/d/1iNrE2Y-s4FInUv5mxN50QkmWUp3t_O9V/view?usp=drive_link",
+    "https://drive.google.com/file/d/1NM79LY-Rlwc9I0NFuoQpNLJqiteif8Ai/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/sasikiran-kola/",
   twitter: "",
   insta: "https://www.instagram.com/mr_sasi_03/",
